75476d724cf55107d2e0048f51d3d009830eb72e
kevinkusky <kevinkusky@gmail.com>
2021-02-02 16:09:17 -0500

    adds history

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,45 +1,51 @@
-import { render } from '@testing-library/react';
 import React, { useState } from 'react';
 
 import {calculateWinner} from '../helper';
 import Board from './Board'
 
 const Game = () => {
-    const [board, setBoard] = useState(Array(9).fill(null));
+    const [boardHistory, setBoardHistory] = useState([Array(9).fill(null)]);
+    const [step, setStep] = useState(0);
     const [xNext, setXNext] = useState(true);
-    const winner = calculateWinner(board);
+    const winner = calculateWinner(boardHistory[step]);
 
     const handleClick = i => {
-        const boardInstance = [...board];
+        const timeInHistroy = boardHistory.slice(0, step + 1);
+        const current = timeInHistroy[step]
+        const tiles = [...current];
 
-        if (winner || boardInstance[i]) return;
+        if (winner || tiles[i]) return;
 
-        boardInstance[i] = xNext ? 'X' : 'O'
-        setBoard(boardInstance);
+        tiles[i] = xNext ? 'X' : 'O'
+        setBoardHistory([...timeInHistroy, tiles]);
+        setStep(timeInHistroy.length);
         setXNext(!xNext);
     }
 
-    const revertTo = () => {
-
+    const revertTo = step => {
+        setStep(step);
+        setXNext(step % 2 === 0);
     }
 
-    const clearBoard = () => (
-        <button onClick={
-            () => {
-                setBoard(Array(9).fill(null));
-                setXNext(true);
-            }}
-        >New Game</button>
+    const renderBoard = () => (
+        boardHistory.map((step, move) => {
+            const destination = move ? `Go to move #${move}` : 'Start New Game';
+            return (
+                <li key={move}>
+                    <button onClick={() => revertTo(move)}>{destination}</button>
+                </li>
+            )
+        })
     )
 
     return(
         <div className='entire-game'>
-            <Board tiles={board} onClick={handleClick} />
+            <Board tiles={boardHistory[step]} onClick={handleClick} />
             <div className='board-controls'>
                 <p>
                     {winner ? 'Winner: ' + winner : 'Next Player: ' + (xNext ? 'X' : 'O')}
                 </p>
-                {clearBoard()}
+                {renderBoard()}
             </div>
         </div>
     )
